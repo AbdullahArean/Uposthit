aeea7a17e93c49711b7ec14c614971ae0d5a635c
Raufun <raufun.nazin13@gmail.com>
2022-10-20 23:07:27 +0600

    fixed css plugins

--- src/pages/single/Single.jsx
+++ src/pages/single/Single.jsx
@@ -11,7 +11,7 @@ const Single = () => {
         <hr className='mx-2 mb-3'/>
         <div className="top">
           <div className="left">
-            <h1 className="name"></h1>
+            <h1 className="name">name</h1>
             <div className="student"><img src="" alt="" className="s_img" /></div>
           </div>
           <div className="right">

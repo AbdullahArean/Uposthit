4f363f3089900e690bacb498f7a198e3b041291c
AhnafTahmiddie <ahnaftahmid2604@gmail.com>
2023-01-04 21:29:03 +0600

    Backend Issue Fixed

--- src/components/dataTable/StudentTable.jsx
+++ src/components/dataTable/StudentTable.jsx
@@ -1,8 +1,8 @@
-import React from "react";
-import { DataGrid } from "@mui/x-data-grid";
-import * as AiIcons from "react-icons/ai";
-import * as FaIcons from "react-icons/fa";
-import { studentColumns, studentRows } from "../tableData/studentData";
+import React from 'react'
+import { DataGrid } from '@mui/x-data-grid';
+import * as AiIcons from 'react-icons/ai'
+import * as FaIcons from 'react-icons/fa'
+import { studentColumns, studentRows } from '../tableData/studentData';
 
 const actionColumn = [
   {
@@ -13,12 +13,12 @@ const actionColumn = [
       return (
         <div className="cellAction text-center grid grid-cols-2 w-full">
           <button className="viewButton hover:bg-violet-200 rounded-lg flex items-center justify-center space-x-2">
-            <p className="text-sm">View</p>
-            <FaIcons.FaShareSquare className="text-violet-800" />
+            <p className='text-sm'>View</p>
+            <FaIcons.FaShareSquare className='text-violet-800'/>
           </button>
           <button className="deleteButton hover:bg-red-200 rounded-lg flex items-center justify-center space-x-2">
             <p className="text-sm">Delete</p>
-            <AiIcons.AiFillDelete className="text-red-800" />
+            <AiIcons.AiFillDelete className='text-red-800'/>
           </button>
         </div>
       )
@@ -46,13 +46,12 @@ const actionColumn = [
 
 const DataTable = ({rows, columns}) => {
 
-const DataTable = ({ data }) => {
   return (
     <div className='text-center mx-7 my-6 border rounded-lg focus:ring-0 focus:border-none'>
       <DataGrid rows={rows} columns={columns}
       />
     </div>
-  );
-};
+  )
 }
-export default DataTable;
+
+export default DataTable
